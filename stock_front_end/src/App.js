96b849c9f02f22e3134910f539b8b7e5afe29c41
jmkhamdevelops-cloud <jmkhamdevelops@gmail.com>
2026-02-07 11:25:35 -0600

    Attempting to send input

--- stock_front_end/src/App.js
+++ stock_front_end/src/App.js
@@ -4,20 +4,31 @@ import './App.css';
 
 function App() {
   const [text, setText] = useState("");
+  const sendToDjango = async () => {
+    const response = await fetch("http://localhost:8000/api/save/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({text}),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Stock App</h1>   
- <input
+      <input
         type="text"
+        style={{padding: "8px", fontSize: "12px", height: "40px", width: "80px"}}
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="Type here..."
       />
-
-      <p>Ticker: {text}</p>
-
-         
+      <p>Ticker: {text}</p> 
       </header>
     </div>
   );
