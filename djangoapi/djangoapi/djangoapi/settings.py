73342bbc804143bd00dff562a3985df7991fb118
jmkhamdevelops-cloud <jmkhamdevelops@gmail.com>
2026-02-07 14:02:39 -0600

    Added middleware

--- djangoapi/djangoapi/djangoapi/settings.py
+++ djangoapi/djangoapi/djangoapi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'tickers',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,9 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoapi.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
