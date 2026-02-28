bf0df9a0d37c808752b8fb3743e919c32551eb3f
jmkhamdevelops-cloud <jmkhamdevelops@gmail.com>
2026-02-27 20:48:51 -0600

    Able to display data from Holdings

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -1,8 +1,10 @@
 import { useAuth } from "../context/AuthContext";
 import { Card, Col, Row, Typography, Button } from "antd";
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
 import { createRoot } from "react-dom/client";
 import { AgCharts } from "ag-charts-react";
+import { getCurrentHoldings } from '../api/holdings';
+
 import { AllCommunityModule, ModuleRegistry, LegendModule, CategoryAxisModule,
         LineSeriesModule, NumberAxisModule} from "ag-charts-community";
 
@@ -44,16 +46,37 @@ const LineChart = () => {
   return <AgCharts options={options} />;
 };
 
-
-
-
-
-
-
 export default function Dashboard() {
     const { me } = useAuth();
+
+    const [holdingData, setHoldingData] = useState([])
+
+    useEffect(() => {
+        async function load() {
+            const data = await getCurrentHoldings();
+            setHoldingData(data);
+        }
+        load();
+    }, []);
     return (
         <Row gutter={[16, 16]}>
+              <Col lg={16}>
+                <Card style={{ width: "100%" }} title="Example All Holdings">
+                    {/* This example shows how to list all of the data from the holding array */}
+                    {holdingData.map((h) => (
+                        <div key={h.id}>
+                            <Text>{h.ticker}: {h.quantity}</Text>
+                        </div>
+                    ))}
+                </Card>
+            </Col>
+
+            <Col lg={8}>
+                <Card style={{ width: "100%" }} title="Holdings">
+                    <div>
+                    </div>
+                </Card>
+            </Col>
                <Col lg={8}>
                 <Card style={{ width: "100%" }} title="APPL">
                     <div style={{ height: 120}}>
