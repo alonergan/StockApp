a0f1ed19f3c9c3cb8f1df3cc558be69a102fa7c4
jmkhamdevelops-cloud <jmkhamdevelops@gmail.com>
2026-04-02 09:32:30 -0500

    Unit Test completed

--- Backend/api/test.py
+++ Backend/api/test.py
@@ -1,25 +1,33 @@
 from django.test import TestCase
 from .models import Stock
 
+
+#Unit Test of Crud
 class StockTestCase(TestCase):
+    #Test for creation of stock, pass if successful, failed otherwise
     def test_create_stock(self):
-        ticker = Stock.objects.create(ticker="CHV", name="Chevron")
-        self.assertEqual(ticker.ticker, "CHV")
-        self.assertEqual(ticker.name, "Chevron")
+        stock = Stock.objects.create(ticker="CHV", name="Chevron")
+        self.assertEqual(stock.ticker, "CHV")
+        self.assertEqual(stock.name, "Chevron")
 
+    # Test to update stock's name and ticker
+    # We then check if name = "TESLA" and if ticker = "TSLA"
+    # If any of these checks failed, test fails, otherwise passes 
+    # NOTE: Update was coded to fail on purpose
     def test_update_stock(self):
-        ticker = Stock.objects.create(ticker="TSLA", name="TESLA")
-        ticker.name = "TESLA"
-        ticker.ticker = "TSLA"
-        ticker.save()
+        stock = Stock.objects.create(ticker="TSLA", name="TESLA")
+        stock.name = "TESLa cars" #This will fail the check
+        stock.save()
 
-        updated_ticker = Stock.objects.get(pk=ticker.pk)
-        self.assertEqual(updated_ticker.name, "TESLA")
-        self.assertEqual(updated_ticker.ticker, "TLSA")
+        updated_stock = Stock.objects.get(pk=stock.pk)
+        self.assertEqual(updated_stock.name, "Tesla motors")
+        self.assertEqual(updated_stock.ticker, "TSLA")
 
+    #Deletion of stock
     def test_delete_product(self):
-        ticker = Stock.objects.create(name="Delete Me", ticker="")
-        ticker.delete()
+        stock = Stock.objects.create(name="Delete Me", ticker="DEL")
+        stock_id = stock.id
+        stock.delete()
 
         with self.assertRaises(Stock.DoesNotExist):
-            Stock.objects.get(name="Delete Me")
+            Stock.objects.get(id=stock_id)
