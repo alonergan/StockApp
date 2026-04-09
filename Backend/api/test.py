69ac4c842a0b3e68c5a1ab809df2e41a3b1795ff
jmkhamdevelops-cloud <jmkhamdevelops@gmail.com>
2026-04-08 20:06:04 -0500

    Adding correct test.py

--- Backend/api/test.py
+++ Backend/api/test.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 from .models import Stock
 
 
 #Unit Test of Crud
-class StockTestCase(TestCase):
+class TestCase(TestCase):
     #Test for creation of stock, pass if successful, failed otherwise
     def test_create_stock(self):
         stock = Stock.objects.create(ticker="CHV", name="Chevron")
@@ -23,6 +28,15 @@ class StockTestCase(TestCase):
         self.assertEqual(updated_stock.name, "Tesla motors")
         self.assertEqual(updated_stock.ticker, "TSLA")
 
+    def test_update_fail(self):
+        stock = Stock.objects.create(ticker="TSLA", name="TESLA")
+        stock.name = "TESLA" #This will pass
+        stock.save()
+
+        updated_stock = Stock.objects.get(pk=stock.pk)
+        self.assertEqual(updated_stock.name, "TESLA")
+        self.assertEqual(updated_stock.ticker, "TSLA")
+
     #Deletion of stock
     def test_delete_product(self):
         stock = Stock.objects.create(name="Delete Me", ticker="DEL")
@@ -31,3 +45,13 @@ class StockTestCase(TestCase):
 
         with self.assertRaises(Stock.DoesNotExist):
             Stock.objects.get(id=stock_id)
+
+    def test_logged_in_user_can_access(self):
+        user = User.objects.create_user(username='test', password='pass')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+
+        response = self.client.get('/api/auth/me/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['username'], 'test')
+
